a661c7820713eb19476d6e6b26b8a276e20ec406
ManulParihar <manul.singh.parihar@gmail.com>
2024-04-18 20:08:17 +0530

    Added public to the existing method

--- src/main/java/com/test/ECKeyManagement.java
+++ src/main/java/com/test/ECKeyManagement.java
@@ -8,7 +8,7 @@ import org.web3j.utils.Numeric;
 
 public class ECKeyManagement {
 
-	static String compressPublicKey(BigInteger pubKey) {
+	public static String compressPublicKey(BigInteger pubKey) {
 		String pubKeyYPrefix = pubKey.testBit(0) ? "03" : "02";
 		String pubKeyHex = pubKey.toString(16);
 		String pubKeyX = pubKeyHex.substring(0, 64);
